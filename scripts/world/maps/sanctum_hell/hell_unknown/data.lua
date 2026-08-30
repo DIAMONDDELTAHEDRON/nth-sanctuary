182c61d6c488eab00150704f0e3344b653f4a957
name = theLucidLibrarian <alexanderhaejunkim@gmail.com>
2026-08-30 11:03:51 -0700

    Hey the Hell Filter bug is finally fixed wouldja look at that-

--- scripts/world/maps/sanctum_hell/hell_unknown/data.lua
+++ scripts/world/maps/sanctum_hell/hell_unknown/data.lua
@@ -1,5 +1,5 @@
 return {
-  version = "1.10",
+  version = "1.11",
   luaversion = "5.1",
   tiledversion = "1.12.2",
   class = "",
@@ -2528,7 +2528,7 @@ return {
           type = "",
           shape = "point",
           x = 40,
-          y = 0,
+          y = -10,
           width = 0,
           height = 0,
           rotation = 0,
